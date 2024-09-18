c32307321c54e3fc5dd76036f2605cd4603ad308
ali <aligadit9192@gmail.com>
2024-09-18 21:53:08 +0500

    My first commit

---
+++ game.ts
@@ -0,0 +1,62 @@
+let userScore = 0;
+let compScore = 0;
+let userScorePara = document.querySelector('#user-score');
+let compScorePara= document.querySelector("#comp-score");
+const choices = document.querySelectorAll(".choice")
+const msg = document.querySelector("#msg")
+
+ const showWinner = (userWin,userChoice,compChoice)=>{
+    if(userWin){
+        msg.innerText= `You Win! Your ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor = "green";
+        userScore++
+        userScorePara.innerText = userScore
+
+    }
+    else{
+        msg.innerText=`You lose! ${compChoice} beats your ${userChoice}`;
+        msg.style.backgroundColor = "red";
+        compScore++
+        compScorePara.innerText = compScore
+    }
+
+ }
+const genCompChoice = ()=>{
+    const options = ["rock","paper","scissor"]
+    const randomIdx= Math.floor(Math.random()*3)
+     return options[randomIdx]
+
+}
+const drawGame = ()=>{
+    msg.innerText= "Game Was Draw, Play Again"
+    msg.style.backgroundColor = "#081b31";
+    
+}
+const playGame=(userChoice)=>{
+    const compChoice = genCompChoice()
+
+    if(userChoice === compChoice){
+        drawGame()
+    }
+    else{
+       let userWin = true;
+       if(userChoice === "rock"){
+        userWin =compChoice === "paper" ? false:true
+       }
+       else if (userChoice === "paper"){
+        userWin= compChoice === "scissor" ? false:true
+       }
+       else(
+        userWin = compChoice === "rock" ? false : true
+       )
+    
+    showWinner(userWin,userChoice,compChoice)
+    }
+    
+}
+choices.forEach((choice)=>{    
+    choice.addEventListener("click",()=>{
+        const userChoice=choice.getAttribute("id")
+        playGame(userChoice)
+    })
+}) 
